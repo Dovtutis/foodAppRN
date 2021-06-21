0c3bd932a58a70df2328f4760570cd6030c346b5
Dovtutis <dovtutis@gmail.com>
2021-06-21 10:59:56 +0300

    Meals filter done and wokring

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,20 +1,28 @@
 import React from "react";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
 import { CATEGORIES } from "../data/dummy-data";
 import MealList from "../components/MealList";
+import { View, StyleSheet } from "react-native";
+import DefaultText from "../components/DefaultText";
 
 const CategoryMealsScreen = (props) => {
   const catId = props.navigation.getParam("categoryId");
-  const availableMeals = useSelector(state => state.meals.filteredMeals);
+  const availableMeals = useSelector((state) => state.meals.filteredMeals);
 
   const displayedMeals = availableMeals.filter(
     (meal) => meal.categoryIds.indexOf(catId) >= 0
   );
 
-  return (
-    <MealList listData={displayedMeals} navigation={props.navigation} />
-  );
+  if (displayedMeals.length === 0) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No meals found, check your filters.</DefaultText>
+      </View>
+    );
+  }
+
+  return <MealList listData={displayedMeals} navigation={props.navigation} />;
 };
 
 CategoryMealsScreen.navigationOptions = (navigationData) => {
@@ -25,5 +33,13 @@ CategoryMealsScreen.navigationOptions = (navigationData) => {
     headerTitle: selectedCategory.title,
   };
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
 
 export default CategoryMealsScreen;
